9ca53e81a798f129b1e29909b0b5f4eff4bdde0c
yansan1050 <yasuyo.okd@gmail.com>
2020-12-12 15:02:31 +0900

    modified:   todo/views.py

--- todo/views.py
+++ todo/views.py
@@ -6,3 +6,14 @@ from django.http import HttpResponse
 def todo(request):
     return HttpResponse( 'Hello!' )
 
+def signupview(request):
+    if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        try:
+            user = User.objects.create_user(username_data, '', password_data)
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーは既に登録されています'})
+    else:
+        return render(request, 'signup.html', {})
+    return render(request, 'signup.html', {})
